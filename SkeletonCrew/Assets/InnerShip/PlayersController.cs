afb376e697f605c0979a6ec72bade921d284715b
brixon118 <brixonchan@gmail.com>
2017-03-17 23:24:13 -0500

    Limit of what I can do: Added rooms and collision; damage is needed next

---
+++ SkeletonCrew/Assets/InnerShip/PlayersController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersController : MonoBehaviour {
+    //private Vector2 velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    public int playerNumber = 1;
+    public float speed = 2;
+    private Vector2 prevVector;
+    public bool controlled = true;
+    private Vector2 velocity;
+    //public GameObject parentObject;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controlled)
+        {
+            velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            GetComponent<Rigidbody2D>().velocity = (velocity * speed);
+            if (!velocity.Equals(Vector2.zero))
+            {
+                prevVector = GetComponent<Rigidbody2D>().velocity.normalized;
+            }
+        }
+        transform.up = prevVector;
+    }
+
+    public void switchControlled()
+    {
+        controlled = !controlled;
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+    }
+}
---
+++ SkeletonCrew/Assets/InnerShip/RoomsBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomsBehavior : MonoBehaviour {
+    public float maxHealth = 100;
+    public float health = 100;
+    public float maxWaterLevel = 100;
+    public float waterLevel = 0;
+    public int playerControlled = 0;
+    private PlayersController playerController;
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+    }
+
+
+    void OnTriggerStay2D(Collider2D other) {
+        health--;
+        if( other.CompareTag("PlayerCharacter"))
+        {
+            if (Input.GetButtonUp("Jump"))
+            {
+                playerController = other.gameObject.GetComponent<PlayersController>();
+                if(playerControlled == 0)
+                {
+                    playerControlled = playerController.playerNumber;
+                    playerController.switchControlled();
+                }
+                else if (playerControlled == playerController.playerNumber)
+                {
+                    playerControlled = 0;
+                    playerController.switchControlled();
+                }
+            }
+        }
+    }
+
+}
