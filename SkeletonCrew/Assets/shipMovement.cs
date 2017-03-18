4ac68d934fc60ab9880b1e6f70b0057cd6fd7bd3
Miles Vesper <mvesper20@gmail.com$ git credential-osxkeychainmilesav@1Milesav@1$ git config --global user.name Miles>
2017-03-18 13:44:28 -0500

    Create movement script and cameras
    dual cameras to be connected to ship objects and a shipMovement script
    (flawed)

--- SkeletonCrew/Assets/shipMovement.cs
+++ SkeletonCrew/Assets/shipMovement.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class shipMovement : MonoBehaviour {
 
+	public float movementSpeed = 30;
+	public float rotationSpeed = 10;
+	public float playerInput;
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		UserInputs ();
 	}
+
+	void UserInputs(){
+		
+		if (playerInput == 1) {
+			transform.Translate (Input.GetAxis ("360_LeftThumbStick") * movementSpeed, 0, 0);
+			transform.Rotate(0, Input.GetAxis("360_LeftThumbStick") * rotationSpeed, 0);
+		}
+
+		if (playerInput == 2) {
+			transform.Translate (0, 0, Input.GetAxis ("360_RightThumbStick") * movementSpeed);
+			transform.Rotate(0, Input.GetAxis("360_RightThumbStick") * rotationSpeed, 0);
+		}
+
+		playerInput = playerControlled;
+	}
+
 }
