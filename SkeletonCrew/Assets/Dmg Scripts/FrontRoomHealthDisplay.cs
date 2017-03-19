48ae90de83ddb60e72b3f7c50c8cc25a4a952ef1
Triston-Lamonte <tlamon3@lsu.edu>
2017-03-18 23:30:17 -0500

    Script For Display Start

---
+++ SkeletonCrew/Assets/Dmg Scripts/FrontRoomHealthDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FrontRoomHealthDisplay : MonoBehaviour
+{
+    public GameObject HealthDisplay;
+    public static Text textie;
+
+    // Use this for initialization
+    void Start()
+    {
+        textie = GetComponent<FrontRoomHealth>;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textie.text = "" + GetComponent<RoomsBehavior>().health;
+    }
+}
