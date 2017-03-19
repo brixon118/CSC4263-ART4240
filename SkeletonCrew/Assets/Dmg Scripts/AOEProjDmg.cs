dc7c940c380e33b899dc92d634e154ac2915dba0
Triston-Lamonte <tlamon3@lsu.edu>
2017-03-19 09:58:53 -0500

    Robin mvp (#1)
    * Limit of what I can do: Added rooms and collision; damage is needed next
    
    * Limit of what I can do: Added rooms and collision; damage is needed next
    
    * Limit of what I can do: Added rooms and collision; damage is needed next
    
    * Limit of what I can do: Added rooms and collision; damage is needed next
    
    * Limit of what I can do: Added rooms and collision; damage is needed next
    
    * Limit of what I can do: Added rooms and collision; damage is needed next
    
    * Limit of what I can do: Added rooms and collision; damage is needed next
    
    * Limit of what I can do: Added rooms and collision; damage is needed next
    
    * Limit of what I can do: Added rooms and collision; damage is needed next
    
    * Limit of what I can do: Added rooms and collision; damage is needed next
    
    * Limit of what I can do: Added rooms and collision; damage is needed next
    
    * Limit of what I can do: Added rooms and collision; damage is needed next
    
    * Limit of what I can do: Added rooms and collision; damage is needed next
    
    * First screw up of the day

---
+++ SkeletonCrew/Assets/Dmg Scripts/AOEProjDmg.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AOEProjDmg : MonoBehaviour
+{
+    public bool WhenLand = false;  //True when ballistic lands, else false
+    Vector3 BallisticProjectilePosition;
+    public float AOERadius = 0.6f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (WhenLand == true)
+        {
+            WhenLand = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Physics.OverlapSphere(BallisticProjectilePosition, AOERadius);
+    }
+}
---
+++ SkeletonCrew/Assets/InnerShip/HallwayBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HallwayBehavior : MonoBehaviour {
+    public float maxHealth = 100;
+    public float health = 100;
+    public float maxWaterLevel = 100;
+    public float waterLevel = 0;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
